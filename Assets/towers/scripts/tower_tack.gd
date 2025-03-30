945309ee66a1551794a391680982a2dfc353402a
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-03-29 20:06:03 -0500

    Spinning top animation updates

--- Assets/towers/scripts/tower_tack.gd
+++ Assets/towers/scripts/tower_tack.gd
@@ -1,6 +1,13 @@
 class_name Tower_Tack extends Area_Towers
 
+@onready var spin_sparks: MeshInstance3D = %Spin_Sparks
+@onready var animation_sparks: AnimationPlayer = %AnimationPlayer_Sparks
+
 var one_shot: bool = false
+
+func _upgrade_radius(radius:float) -> void:
+	super._upgrade_radius(radius)
+	spin_sparks.mesh.size = Vector2(radius*2,radius*2)
 
 func _on_attack_moment() -> void:
 	do_attack(null)
@@ -9,13 +16,16 @@ func do_attack(_target) -> void:
 	for target in detected_targets:
 		if target.target_level < 0:
 			#print("TACK_TOWER: dead target temporarily targeted")
-			return
-		else:
-			target.take_single_damage()
+			continue
+		
+		target.take_single_damage()
+		if not mesh_only:
 			animate_attack()
 	
 func animate_attack() -> void:
 	animation.set("parameters/attack/request", 1)
+	animation_sparks.play("show_sparks")
 
 func unanimate_attack() -> void:
 	animation.set("parameters/attack/request", 3)
+	
